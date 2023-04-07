6a31bec68749aa26b100decd4c2d3418a53037a2
Wahliyudin <wahliyudinzein@gmail.com>
2023-04-07 19:36:00 +0700

    Membuat master template

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,154 @@
+<!DOCTYPE html>
+<html lang="en">
+<meta http-equiv="content-type" content="text/html;charset=UTF-8" />
+
+<head>
+    <title>Craft | Bootstrap 5 HTML Admin Dashboard Theme by Keenthemes</title>
+    <meta charset="utf-8" />
+    <meta name="description"
+        content="Craft admin dashboard live demo. Check out all the features of the admin panel. A large number of settings, additional services and widgets." />
+    <meta name="keywords"
+        content="Craft, bootstrap, bootstrap 5, admin themes, dark mode, free admin themes, bootstrap admin, bootstrap dashboard" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta property="og:locale" content="en_US" />
+    <meta property="og:type" content="article" />
+    <meta property="og:title" content="Craft - Bootstrap 5 HTML Admin Dashboard Theme" />
+    <meta property="og:url" content="https://themes.getbootstrap.com/product/craft-bootstrap-5-admin-dashboard-theme" />
+    <meta property="og:site_name" content="Keenthemes | Craft" />
+    <link rel="canonical" href="https://preview.keenthemes.com/craft" />
+    <link rel="shortcut icon" href="assets/media/logos/favicon.ico" />
+
+    <!--begin::Fonts(mandatory for all pages)-->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Inter:300,400,500,600,700" />
+    <!--end::Fonts-->
+
+    <!--begin::Global Stylesheets Bundle(mandatory for all pages)-->
+    <link href="{{ asset('assets/plugins/global/plugins.bundle.css') }}" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('assets/css/style.bundle.css') }}" rel="stylesheet" type="text/css" />
+    <!--end::Global Stylesheets Bundle-->
+
+    @stack('css')
+</head>
+<!--end::Head-->
+
+<!--begin::Body-->
+
+<body id="kt_body"
+    class="header-fixed header-tablet-and-mobile-fixed toolbar-enabled aside-fixed aside-default-enabled">
+
+
+    <!--begin::Main-->
+    <!--begin::Root-->
+    <div class="d-flex flex-column flex-root">
+        <!--begin::Page-->
+        <div class="page d-flex flex-row flex-column-fluid">
+            <!--begin::Aside-->
+            @include('layouts.master.side')
+            <!--end::Aside-->
+
+            <!--begin::Wrapper-->
+            <div class="wrapper d-flex flex-column flex-row-fluid" id="kt_wrapper">
+                <!--begin::Header-->
+                @include('layouts.master.header')
+                <!--end::Header-->
+
+                <!--begin::Content-->
+                <div class="content fs-6 d-flex flex-column flex-column-fluid" id="kt_content">
+                    <!--begin::Toolbar-->
+                    <div class="toolbar" id="kt_toolbar">
+                        <div class=" container-fluid  d-flex flex-stack flex-wrap flex-sm-nowrap">
+                            <!--begin::Info-->
+                            <div class="d-flex flex-column align-items-start justify-content-center flex-wrap me-2">
+                                <!--begin::Title-->
+                                <h1 class="text-dark fw-bold my-1 fs-2">
+                                    Dashboard <small class="text-muted fs-6 fw-normal ms-1"></small>
+                                </h1>
+                                <!--end::Title-->
+
+                                <!--begin::Breadcrumb-->
+                                <ul class="breadcrumb fw-semibold fs-base my-1">
+                                    <li class="breadcrumb-item text-muted">
+                                        <a href="index-2.html" class="text-muted text-hover-primary">
+                                            Home </a>
+                                    </li>
+
+                                    <li class="breadcrumb-item text-muted">
+                                        Dashboards </li>
+
+                                    <li class="breadcrumb-item text-dark">
+                                        Multipurpose </li>
+
+                                </ul>
+                                <!--end::Breadcrumb-->
+                            </div>
+                            <!--end::Info-->
+
+                            <!--begin::Actions-->
+                            <div class="d-flex align-items-center flex-nowrap text-nowrap py-1">
+                                <a href="#" class="btn bg-body btn-color-gray-700 btn-active-primary me-4"
+                                    data-bs-toggle="modal" data-bs-target="#kt_modal_invite_friends">
+                                    Invite Friends
+                                </a>
+
+                                <a href="#" class="btn btn-primary" data-bs-toggle="modal"
+                                    data-bs-target="#kt_modal_create_project" id="kt_toolbar_primary_button">
+                                    New Project </a>
+                            </div>
+                            <!--end::Actions-->
+                        </div>
+                    </div>
+                    <!--end::Toolbar-->
+
+                    @yield('content')
+                </div>
+                <!--end::Content-->
+
+                <!--begin::Footer-->
+                @include('layouts.master.footer')
+                <!--end::Footer-->
+            </div>
+            <!--end::Wrapper-->
+        </div>
+        <!--end::Page-->
+    </div>
+    <!--end::Root-->
+
+    <!--begin::Javascript-->
+    <script>
+        var hostUrl = "assets/index.html";
+    </script>
+
+    <!--begin::Global Javascript Bundle(mandatory for all pages)-->
+    <script src="{{ asset('assets/plugins/global/plugins.bundle.js') }}"></script>
+    <script src="{{ asset('assets/js/scripts.bundle.js') }}"></script>
+    <!--end::Global Javascript Bundle-->
+
+    <!--begin::Theme mode setup on page load-->
+    <script>
+        var defaultThemeMode = "light";
+        var themeMode;
+
+        if (document.documentElement) {
+            if (document.documentElement.hasAttribute("data-bs-theme-mode")) {
+                themeMode = document.documentElement.getAttribute("data-bs-theme-mode");
+            } else {
+                if (localStorage.getItem("data-bs-theme") !== null) {
+                    themeMode = localStorage.getItem("data-bs-theme");
+                } else {
+                    themeMode = defaultThemeMode;
+                }
+            }
+
+            if (themeMode === "system") {
+                themeMode = window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light";
+            }
+
+            document.documentElement.setAttribute("data-bs-theme", themeMode);
+        }
+    </script>
+    <!--end::Theme mode setup on page load-->
+    @stack('js')
+</body>
+<!--end::Body-->
+
+</html>
